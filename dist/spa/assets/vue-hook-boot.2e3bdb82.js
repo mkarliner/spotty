1faecdfeaa1e050d035fca659d818c0dd06d18f8
Michael Karliner <mike@modern-industry.com>
2022-09-15 15:16:42 +0100

    build

--- dist/spa/assets/vue-hook-boot.2e3bdb82.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.20661ed9.js";import{m as r}from"./mqtt-vue-hook.es.f3d9172b.js";var e="wss",a="mqtt.pskreporter.info",s=1886,p=t(async({app:o,router:m})=>{o.use(r,`${e}://${a}:${s}`,{clean:!1,keepalive:60,clientId:`mqtt_client_${Math.random().toString(16).substring(2,10)}`,connectTimeout:4e3})});export{p as default};
