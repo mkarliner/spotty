97c8529ca9eb13b4041861bbe7164ed7e9e6e2a7
Michael Karliner <mike@modern-industry.com>
2022-09-20 14:32:34 +0100

    Build and deploy

---
+++ dist/spa/assets/global.3be044b4.js
@@ -0,0 +1,1 @@
+import{b as o,E as e}from"./index.7f738375.js";window.global||(window.global=window);console.log("GLOBAL!");window.Buffer=o.Buffer;window.EventEmitter=e;
--- src/boot/global.js
+++ src/boot/global.js
@@ -1,2 +1,8 @@
 window.global ||= window;
 console.log("GLOBAL!")
+   import EventEmitter from "events";
+      import { Buffer } from "buffer";
+
+      window.Buffer = Buffer;
+      // window.process = p;
+      window.EventEmitter = EventEmitter;
