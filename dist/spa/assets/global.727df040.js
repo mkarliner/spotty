ad44add80f8122962fb3c0f87314eeb6c7de81e0
Michael Karliner <mike@modern-industry.com>
2022-10-17 14:25:04 +0100

    BUILD/DEPLOY

---
+++ dist/spa/assets/global.727df040.js
@@ -0,0 +1,1 @@
+import{b as o,E as e}from"./index.912a60ac.js";window.global||(window.global=window);console.log("GLOBAL!");window.Buffer=o.Buffer;window.EventEmitter=e;
--- dist/spa/assets/use-dark.16313680.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index.3f315e14.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
