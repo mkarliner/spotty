7cb341f2104670b74091343c3e976b2a7fe50b99
Michael Karliner <mike@modern-industry.com>
2022-10-22 10:49:10 +0100

    BUILD/DEPLOY

--- dist/spa/assets/global.727df040.js
+++
@@ -1,1 +0,0 @@
-import{b as o,E as e}from"./index.912a60ac.js";window.global||(window.global=window);console.log("GLOBAL!");window.Buffer=o.Buffer;window.EventEmitter=e;
---
+++ dist/spa/assets/settings.a37d8fa9.js
@@ -0,0 +1,1 @@
+import{R as e}from"./index.8e181aba.js";const r=e("settings",{state:()=>({topic:"pskr/filter/v2/+/+/+/+/IO91/#",report_points:{},report_ttl:60,callsign:"G8LKD",grid:"IO91",mode:"grid"}),getters:{getTopic(t){return t.topic},getReportPoints(t){return console.log("sss"),t.report_points}},actions:{setTopic(t){this.topic=t}}});export{r as u};
