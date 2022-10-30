f72ba9cf79a3a868026a1516828a7f847c284717
Michael Karliner <mike@modern-industry.com>
2022-10-30 10:41:53 +0000

    BUILD/DEPLOY

---
+++ dist/spa/assets/global.ae2842ce.js
@@ -0,0 +1,1 @@
+import{b as o,E as e}from"./index.b8d17992.js";window.global||(window.global=window);console.log("GLOBAL!");window.Buffer=o.Buffer;window.EventEmitter=e;
---
+++ dist/spa/assets/vue-hook-boot.f633235a.js
@@ -0,0 +1,1 @@
+import{k as t}from"./index.b8d17992.js";import{m as r}from"./mqtt-vue-hook.es.f3d9172b.js";var e="wss",a="mqtt.pskreporter.info",s=1886,p=t(async({app:o,router:m})=>{o.use(r,`${e}://${a}:${s}`,{clean:!1,keepalive:60,clientId:`mqtt_client_${Math.random().toString(16).substring(2,10)}`,connectTimeout:4e3})});export{p as default};
