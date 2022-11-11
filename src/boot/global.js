89092205f5271250e0155de65534de003f2fc0fc
Michael Karliner <mike@modern-industry.com>
2022-11-11 11:27:56 +0000

    B/D

--- src/boot/global.js
+++
@@ -1,8 +0,0 @@
-window.global ||= window;
-console.log("GLOBAL!")
-   import EventEmitter from "events";
-      import { Buffer } from "buffer";
-
-      window.Buffer = Buffer;
-      // window.process = p;
-      window.EventEmitter = EventEmitter;
